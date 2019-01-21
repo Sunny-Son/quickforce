dad4f78abf81b07feb89768f74a24068de75c576
Sunny-Son <insun.son@gmail.com>
2019-01-21 18:40:48 +0900

    first commit

--- app.js
+++ app.js
@@ -33,7 +33,8 @@ app.get('/', function(req, res) {
       // authenticated
       org.authenticate(req.query, function(err) {
         if (!err) {
-          org.query({ query: 'SELECT id, name, type, industry, rating FROM Account' }, function(err, results) {
+//          org.query({ query: 'SELECT id, name, type, industry, rating FROM Account' }, function(err, results) {
+          org.query({ query: 'SELECT id, name, productcode, description, family FROM product2' }, function(err, results) {
             if (!err) {
               res.render('index', {records: results.records});
             }
