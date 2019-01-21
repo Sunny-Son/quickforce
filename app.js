a7bcb00f93cd57dcfefd6fb4b25be130375df848
Sunny-Son <insun.son@gmail.com>
2019-01-21 20:28:11 +0900

    first commit

--- app.js
+++ app.js
@@ -22,54 +22,6 @@ hbs.registerHelper('get', function(field) {
   return this.get(field);
 });
 
-app.get('/update', function(req, res) {
-  if (isSetup()) {
-    var org = nforce.createConnection({
-      clientId: process.env.CONSUMER_KEY,
-      clientSecret: process.env.CONSUMER_SECRET,
-      redirectUri: oauthCallbackUrl(req),
-      mode: 'single'
-    });
-
-    const product_name="SUNNY";
-    const product_id="01t0o0000090m7SAAQ";
-
-    if (req.query.code !== undefined) {
-      // authenticated
-      org.authenticate(req.query, function(err) {
-        if (!err) {
-//          org.query({ query: 'SELECT id, name, type, industry, rating FROM Account' }, function(err, results) {
-//          org.query({ query: 'SELECT id, name, productcode, description, family FROM product2' }, function(err, results) {
-          org.query({ query: 'UPDATE product2 SET NAME = \'SUNNY\' WHERE id=\'01t0o0000090m7SAAQ\' ' },function(err, results) {
-//          org.query({ query: 'UPDATE product2 SET NAME = ${product_name} WHERE id=${product_id}' },function(err, results) {
-            if (!err) {
-//              res.render('index', {records: results.records});
-              res.send('updated');
-            }
-            else {
-              res.send(err.message);
-            }
-          });
-        }
-        else {
-          if (err.message.indexOf('invalid_grant') >= 0) {
-            res.redirect('/');
-          }
-          else {
-            res.send(err.message);
-          }
-        }
-      });
-    }
-    else {
-      res.redirect(org.getAuthUri());
-    }
-  }
-  else {
-    res.redirect('/setup');
-  }
-});
-
 app.get('/', function(req, res) {
   if (isSetup()) {
     var org = nforce.createConnection({
@@ -123,6 +75,54 @@ app.get('/setup', function(req, res) {
       herokuApp = req.hostname.replace(".herokuapp.com", "");
     }
     res.render('setup', { isLocal: isLocal, oauthCallbackUrl: oauthCallbackUrl(req), herokuApp: herokuApp});
+  }
+});
+
+app.get('/update', function(req, res) {
+  if (isSetup()) {
+    var org = nforce.createConnection({
+      clientId: process.env.CONSUMER_KEY,
+      clientSecret: process.env.CONSUMER_SECRET,
+      redirectUri: oauthCallbackUrl(req),
+      mode: 'single'
+    });
+
+    const product_name="SUNNY";
+    const product_id="01t0o0000090m7SAAQ";
+
+    if (req.query.code !== undefined) {
+      // authenticated
+      org.authenticate(req.query, function(err) {
+        if (!err) {
+//          org.query({ query: 'SELECT id, name, type, industry, rating FROM Account' }, function(err, results) {
+//          org.query({ query: 'SELECT id, name, productcode, description, family FROM product2' }, function(err, results) {
+          org.query({ query: 'UPDATE product2 SET NAME = \'SUNNY\' WHERE id=\'01t0o0000090m7SAAQ\' ' },function(err, results) {
+//          org.query({ query: 'UPDATE product2 SET NAME = ${product_name} WHERE id=${product_id}' },function(err, results) {
+            if (!err) {
+//              res.render('index', {records: results.records});
+              res.send('updated');
+            }
+            else {
+              res.send(err.message);
+            }
+          });
+        }
+        else {
+          if (err.message.indexOf('invalid_grant') >= 0) {
+            res.redirect('/');
+          }
+          else {
+            res.send(err.message);
+          }
+        }
+      });
+    }
+    else {
+      res.redirect(org.getAuthUri());
+    }
+  }
+  else {
+    res.redirect('/setup');
   }
 });
 
