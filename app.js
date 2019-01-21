deb9c832f52b2791b64239391935bdd3c58ebbc2
Sunny-Son <insun.son@gmail.com>
2019-01-21 18:55:43 +0900

    first commit

--- app.js
+++ app.js
@@ -20,6 +20,54 @@ hbs.registerHelper('get', function(field) {
   return this.get(field);
 });
 
+app.put('/update/:id', function(req, res) {
+  if (isSetup()) {
+    var org = nforce.createConnection({
+      clientId: process.env.CONSUMER_KEY,
+      clientSecret: process.env.CONSUMER_SECRET,
+      redirectUri: oauthCallbackUrl(req),
+      mode: 'single'
+    });
+
+    const id = parseInt(req.params.id)
+    const { productcode, product_name, product_description, product_category } = req.body
+
+    if (req.query.code !== undefined) {
+      // authenticated
+      org.authenticate(req.query, function(err) {
+        if (!err) {
+/*
+          org.query({ query: 'SELECT id, name, productcode, description, family FROM product2' }, function(err, results) {
+            if (!err) {
+              res.render('index', {records: results.records});
+            }
+            else {
+              res.status(200).send(`working : ${id} , ${productcode}, ${product_name}, ${product_description}, ${product_category}`);
+            }
+          });
+
+*/
+          res.status(200).send(`working : ${id} , ${productcode}, ${product_name}, ${product_description}, ${product_category}`);
+        }
+        else {
+          if (err.message.indexOf('invalid_grant') >= 0) {
+            res.redirect('/');
+          }
+          else {
+            res.send(err.message);
+          }
+        }
+      });
+    }
+    else {
+      res.redirect(org.getAuthUri());
+    }
+  }
+  else {
+    res.redirect('/setup');
+  }
+});
+
 app.get('/', function(req, res) {
   if (isSetup()) {
     var org = nforce.createConnection({
